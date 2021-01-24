6c9dbf9484704e4ee720c23275dfc2f61200547b
lauravangorp <lauravangorp@users.noreply.github.com>
2021-01-24 19:49:12 +0100

    finale

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-      let scrollPositiePx = window.pageYOffset;
-      console.log(scrollPositiePx);
-      if (scrollPositiePx > 160) {
-            document.querySelector("header").classList.add("fixed");
-      } else {
-            document.querySelector("header").classList.remove("fixed");
-      };
-})
